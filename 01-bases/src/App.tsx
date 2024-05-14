854e11e07a525966612a72e5304039de9e83bd25
Luis Miguel <luismiguelpaz96@gmail.com>
2024-05-13 20:38:03 -0500

    ✨FEATURE: create new component CounterBy

--- 01-bases/src/App.tsx
+++ 01-bases/src/App.tsx
@@ -1,9 +1,12 @@
 import { Counter } from './bases/Counter';
+import { CounterBy } from './bases/CounterBy';
+import { CounterEffect } from './bases/CounterEffect';
 
 function App() {
   return (
     <>
       <Counter />
+      <CounterBy />
     </>
   );
 }
