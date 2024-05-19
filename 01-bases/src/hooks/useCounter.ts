c02b04df0a9d8d32e90635986e002fb03b1d02be
Luis Miguel <luismiguelpaz96@gmail.com>
2024-05-19 08:43:03 -0500

    ✨FEATURE: creating new hook

---
+++ 01-bases/src/hooks/useCounter.ts
@@ -0,0 +1,30 @@
+import { useEffect, useRef, useState } from "react"
+import gsap from 'gsap';
+
+const MAXIMUM_COUNT = 10;
+
+export const useCounter = () => {
+    const [counter, setCounter] = useState(5)
+    const counterElement = useRef<HTMLHeadingElement>(null)
+
+    const handleClick =  () => {
+        setCounter( prev => Math.min( prev + 1 ,MAXIMUM_COUNT ))
+    }
+
+    useEffect(() => {
+
+        if (counter < 10 ) return;
+
+        const tl = gsap.timeline()
+
+        tl.to(counterElement.current,{ y: -10, duration: 0.2, ease: 'ease.out'})
+        tl.to(counterElement.current,{ y: 0, duration: 1, ease: 'bounce.out'})
+        
+    }, [counter])
+
+    return {
+        counter,
+        counterElement,
+        handleClick
+    }  
+}
