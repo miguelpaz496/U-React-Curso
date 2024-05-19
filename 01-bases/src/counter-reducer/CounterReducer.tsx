54fb55bb2c0698a46598e7458c8db20d001bd734
Luis Miguel <luismiguelpaz96@gmail.com>
2024-05-19 10:12:59 -0500

    ✨FEATURE: action, interfaces and reducer files

--- 01-bases/src/counter-reducer/CounterReducer.tsx
+++ 01-bases/src/counter-reducer/CounterReducer.tsx
@@ -1,44 +1,11 @@
 import { useReducer } from 'react'
-
-
-interface CounterState {
-    counter: number,
-    previous: number,
-    changes: number
-}
+import { CounterState } from './interfaces/interfaces';
+import { counterReducer } from './state/counterReducer';
 
 const INICIAL_STATE: CounterState = {
     counter: 0,
     previous: 0,
     changes: 0
-}
-
-type CounterAction = 
-    |{ type: 'increaseBy', payload: { value: number } }
-    |{ type: 'reset' }
-
-const counterReducer = ( state: CounterState, action: CounterAction): CounterState => {
-
-    switch (action.type) {
-        case  'increaseBy':
-            
-            return {
-                counter: state.counter + action.payload.value,
-                previous: state.counter,
-                changes: state.changes + 1
-            };
-        case  'reset':
-            return {
-                counter: 0,
-                previous: 0,
-                changes: 0
-            }
-            ;
-    
-        default:
-            return state;
-    }
-
 }
 
 export const CounterReducerComponent = () => {
@@ -55,7 +22,7 @@ export const CounterReducerComponent = () => {
 
     return (
         <>
-            <h1>Counter Reducer </h1>
+            <h1>Counter Reducer Segmentado </h1>
             <pre>
                 {JSON.stringify(counterState,null,2)}
             </pre>
